d89c73fd4581edff0e4c92421792ddc40ff5a785
jarc1010 <jaircarlys@gmail.com>
2018-10-26 23:50:55 -0500

    Servidor indexjarc.js para listar usuario bd sql server

--- indexjarc.js
+++ indexjarc.js
@@ -1,9 +1,45 @@
 
 const express = require('express');
+//const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
+//app.use(morgan('dev'));
+
+//conexion a sql server
+const sql = require('mssql')
+const config = {
+  user: 'sa',
+  password: 'Jarc3424',
+  server: 'JARC-PC', 
+  database: 'KSMSALUD',
+}
+
+//rutas
 app.get('/', function(req , res){
-    res.end('Hola Mundo JS');
+    console.log('Hola Mundo JS, peticion de la ruta /: '+ req.url);
+    (async function () {
+        try {
+            let pool = await sql.connect(config)
+            let result = await pool.request()
+            .input('USUARIO', sql.VarChar, 'JRIOS')
+            // .query('SELECT * FROM USUSU WHERE USUARIO= @USUARIO')
+            .query('SELECT * FROM USUSU ')
+            
+            console.dir("Resultado: "+ JSON.stringify({ success: true, result: result.recordset }))
+            res.end(JSON.stringify({ success: true, result: result.recordset }));
+        } catch (err) {
+            console.error("Error: "+err)
+        } finally{
+            sql.close();
+        }
+    })() 
+})
+
+app.get('*', function(req , res){
+    console.log('Hola Mundo JS, peticion de ruta desconocida '+ req.originalUrl);
+    res.end('Hola Mundo JS, peticion de ruta desconocida: '+ req.originalUrl);
 });
 
 app.listen(3000, function(){
